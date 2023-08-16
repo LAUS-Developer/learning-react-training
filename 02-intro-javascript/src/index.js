cdaca09ccc31ef1499d57fe22c7cf0110466d3f3
LAUS Developer <lausdeveloper@outlook.com>
2023-08-16 16:41:12 -0500

    02-I-JS-Practica1

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,1 +1,24 @@
-console.log("hola mundo")
+console.log("hola mundo");
+
+const nombre = "fernando";
+const apellido = "herrera";
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log (nombre, apellido, valorDado)
+
+
+if (true){
+    let valorDado = 6;
+    console.log (valorDado)
+}
+console.log (valorDado)
+
+
+
+function getSaludo (){
+    return "hola mundo";
+}
+
+console.log (`"este es un texto: ${getSaludo()}"`)
